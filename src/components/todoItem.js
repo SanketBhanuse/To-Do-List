a38ba59df4534864791be55c06616ef2044efb80
SanketBhanuse <sanketbhanuse9@gmail.com>
2024-05-13 00:58:56 +0530

    update links

--- src/components/todoItem.js
+++ src/components/todoItem.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
-const todoItem = ({todoitem, onDelete}) => {
+const todoItem = ({ todoitem, onDelete }) => {
   return (
     <section id="todo_item">
-        <div className="todo_item_container">
-            <div className="task">{todoitem.title}</div>
-            <div className="tadk_description">{todoitem.description}</div>
-            <a href="#" className='del_btn' onClick={()=>{onDelete(todoitem)}}>Delete</a>
-        </div>
+      <div className="todo_item_container">
+        <div className="task">{todoitem.title}</div>
+        <div className="tadk_description">{todoitem.description}</div>
+        <div className='del_btn' onClick={() => { onDelete(todoitem) }}>Delete</div>
+      </div>
     </section>
   )
 }
